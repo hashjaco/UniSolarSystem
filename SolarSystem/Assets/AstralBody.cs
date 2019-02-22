ce49263fe30918883fc2d39ddb9e83744d227693
Paul <puhlenko@mail.sfsu.edu>
2019-02-22 12:12:05 -0800

    Created initial Solar System Structure. Added Textures

---
+++ SolarSystem/Assets/AstralBody.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstralBody : MonoBehaviour{
+
+    public Vector3 rotatationSpeed;
+    public Transform bodyTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotate();
+    }
+    public void rotate()
+    {
+        bodyTransform.Rotate(rotatationSpeed*Time.deltaTime);
+    }
+}
