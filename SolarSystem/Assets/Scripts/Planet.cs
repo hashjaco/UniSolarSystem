6437afb1a606a523555eb6e195d34c91d150bc9e
Paul <puhlenko@mail.sfsu.edu>
2019-02-23 15:51:41 -0800

    Added particle effects to saturn. Gave each planet spin and rotation. Added sceond Earth and Moon scene. Can now transition between earth and solar system with right arrow. Can turn off sun with spacebar

---
+++ SolarSystem/Assets/Scripts/Planet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : AstralBody
+{
+    // Start is called before the first frame update
+    public Transform center;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = RotatePointAroundPivot(transform.position,
+                            transform.parent.position,
+                            Quaternion.Euler(0, speed* Time.deltaTime, 0));
+        rotate();
+    }
+    public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle)
+    {
+        return angle * (point - pivot) + pivot;
+    }
+}
---
+++ SolarSystem/Assets/Scripts/TurnOffSun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffSun : MonoBehaviour
+
+    
+{
+    public GameObject sun;
+    private Light light;
+    private Behaviour halo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = sun.GetComponent<Light>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)){
+
+            light.enabled = !light.enabled;
+            sun.GetComponent<Behaviour>().enabled = !sun.GetComponent<Behaviour>().enabled;
+        }
+    }
+}
